dc56ad16ec9ee0153ac36bc73ee5bdf0040cc24d
조기홍 <gihong0409@naver.com>
2025-11-18 20:21:48 +0900

    git init

---
+++ src/main/java/com/willbe/willbecommon/WillbeCommonApplication.java
@@ -0,0 +1,13 @@
+package com.willbe.willbecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillbeCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WillbeCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/willbe/willbecommon/WillbeCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willbe.willbecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillbeCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
